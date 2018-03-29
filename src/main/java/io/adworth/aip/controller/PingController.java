ce9c80ad5c9907a8f8f69fddcd09402a3540dc2e
Orix <orix.auyeung@hunt.social>
2018-03-29 10:44:12 +0800

    Added a json response to report Status OK to RequestMapping("/health").

--- src/main/java/io/adworth/aip/controller/PingController.java
+++ src/main/java/io/adworth/aip/controller/PingController.java
@@ -1,8 +1,16 @@
 package io.adworth.aip.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class PingController {
-
+	
+	@RequestMapping("/health")
+	public Map<String, String> status(){
+		return Collections.singletonMap("Status", "OK");
+	}
 }
