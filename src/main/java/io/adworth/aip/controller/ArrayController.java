a23cbe5a04aa8464ccbaf43a8996038b6c6ead3b
Orix <orix.auyeung@hunt.social>
2018-04-03 14:08:52 +0800

    added synchronized statement to prevent race condition, using a temp array to verify the validity of input before adding the elements into the array & changed the message thrown out on exception  to be more helpful to user.

--- src/main/java/io/adworth/aip/controller/ArrayController.java
+++ src/main/java/io/adworth/aip/controller/ArrayController.java
@@ -3,7 +3,6 @@ package io.adworth.aip.controller;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,40 +11,44 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.adworth.aip.helper.ResponseMessage;
 
 @RestController
+@RequestMapping("/array")
 public class ArrayController {
 	
 	private static ArrayList<Integer> intList = new ArrayList<Integer>();
 	
-	@PutMapping(value = "/array/put")
+	@PutMapping(value = "/put")
 	public ResponseEntity<?> putArray(@RequestBody String json_str){
 		try {
 			JSONObject obj = new JSONObject(json_str);
 			JSONArray arr = obj.getJSONArray("array");
+			ArrayList<Integer> rectified_arr = new ArrayList<Integer>();
 			for ( int i = 0; i < arr.length(); i++) {
 				if (arr.get(i) instanceof Integer){
-					intList.add(arr.getInt(i));
+					rectified_arr.add(arr.getInt(i));
 				} else{
-					intList.add(Integer.parseInt(arr.getString(i)));
+					rectified_arr.add(Integer.parseInt(arr.getString(i)));
 				}
 			}
+			synchronized (intList){
+				intList.addAll(rectified_arr);
+			}
 		} catch (Exception e){
-			return ResponseMessage.response(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
+			return ResponseMessage.response("One or more element(s) cannot be parsed into integers.", HttpStatus.BAD_REQUEST);
 		}
 		return ResponseMessage.response("PUT successful, " + intList, HttpStatus.OK);
 	}
 	
-	@PostMapping("/array/post/{num}")
-	public ResponseEntity<?> postArray(){
+	@PostMapping()
+	public synchronized ResponseEntity<?> postArray(){
 		return null;
 	}
 	
-	@GetMapping("/array")
+	@GetMapping()
 	public ResponseEntity<?> getArray(){
 		return null;
 	}
